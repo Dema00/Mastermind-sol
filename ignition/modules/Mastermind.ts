011ee541b11fd1c689008a34834ae39857dab856
EdoardoColi <edoardo.coli98@gmail.com>
2024-05-29 23:32:11 +0200

    test deploy Mm

---
+++ ignition/modules/Mastermind.ts
@@ -0,0 +1,22 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const MastermindModule = buildModule("MastermindModule", (m) => {
+  // Deploy the MastermindHelper library
+  const mastermindHelper = m.library("MastermindHelper");
+  // const lobbyFunction = m.library("LobbyFunction");
+  // const gameFunction = m.library("GameFunction");
+
+  // Deploy the Mastermind contract and link the libraries
+  const mastermind = m.contract("Mastermind", [], {
+    libraries: {
+      MastermindHelper: mastermindHelper,
+      // LobbyFunction: lobbyFunction,
+      // GameFunction: gameFunction,
+    },
+  });
+
+  // return { mastermindHelper, lobbyFunction, gameFunction, mastermind };
+  return { mastermindHelper, mastermind };
+});
+
+export default MastermindModule;
