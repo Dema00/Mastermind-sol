3db50c66c0a2beb7fa21776c948bae66b4924dba
Dema00 <dema199900@gmail.com>
2024-05-31 19:04:56 +0200

    Continue impl of PatriciaTrie retrieval

--- contracts/lib/Patricia.sol
+++ contracts/lib/Patricia.sol
@@ -5,11 +5,21 @@ import "./GameState.sol";
 
 error NodeNotFound();
 
+
+/**
+ * @title Functions to retrieve and append nodes to a binary Patricia Trie 
+ * 
+ * Nodes are 8 bytes long subdivied as such:
+ *      [++++----][++++----][++++----][++++----][++++----][++++][----++++][----][++++][----]
+ *      └────────────────────────────┘└────────┘└──────────────┘└──────────────┘└────┘└────┘
+ *                  Node address        Padding     Child0 addr     Child1 addr   CC    NC     
+ */
 library Tree {
 
-    function retrieve(Turn storage _turn, bytes8 key, uint8 guess_len ) internal view returns (bytes1 cc_nc){
+    function retrieve(Turn storage _turn, bytes8 key, uint8 guess_len ) 
+    internal view returns (bytes1 cc_nc){
         bytes8 node = _turn.nodes[_turn.root_node];
-        uint8 key_explored = 0;
+        uint8 key_explored;
 
         // If the tree has no more nodes stop
         while (
@@ -19,24 +29,19 @@ library Tree {
             uint8 padding = uint8(bytes1(node >> 24));
             bytes3 mask = bytes3(uint24(1) << padding -1);
             key_explored += padding;
-            if (
-                bytes3(node) ^ mask == 0x0 &&
-                 key_explored < guess_len
-            ) {
+            if (bytes3(node) ^ mask == 0x0) {
                 node = _turn.nodes[bytes2(
                     node >> (32 + 12 * uint8(bytes1(
                         (node >> (key_explored-7)) & bytes1(uint8(0x1))
                     )))
                 ) & bytes2(0xfff0)];
-            } else if (
-                bytes3(node) ^ mask == 0x0 && 
-                key_explored >= guess_len
-            ) {
-                return bytes1(node >> 56);
-            } else {
-                revert NodeNotFound();
             }
+        }
 
+        if (node != 0) {
+            return bytes1(node >> 56);
+        } else {
+            revert NodeNotFound();
         }
     }
 }
