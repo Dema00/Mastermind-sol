8f349fe9f50424bf92dfe9ba4fb3f5b70c9ff448
EdoardoColi <edoardo.coli98@gmail.com>
2024-06-06 22:33:50 +0200

    ok ma problema con hre.ethers.utils

--- test/Mastermind.ts
+++ test/Mastermind.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import hre from "hardhat";
+import hre, { ethers } from "hardhat";
 import { time, loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { AddressLike, Contract, ContractMethodArgs, ContractTransactionReceipt, ContractTransactionResponse, EventLog, Filter, JsonRpcProvider, Listener, Log } from "ethers";
 import { TypeChainEthersContractByName } from "@nomicfoundation/hardhat-ignition-ethers/dist/src/ethers-ignition-helper";
@@ -9,6 +9,7 @@ import { Mastermind } from "../typechain-types";
 import { HardhatEthersProvider } from "@nomicfoundation/hardhat-ethers/internal/hardhat-ethers-provider";
 import { match } from "assert";
 import { connect } from "http2";
+import { getRandomValues } from "crypto";
 
 function findEvent(
     receipt:  ContractTransactionReceipt | null, 
@@ -120,7 +121,7 @@ describe("Mastermind", function () {
 
         await opponent.execFunction("joinGame",[gameId]);
 
-        return { creator, opponent, gameId, manager };
+        return { creator, opponent, gameId, manager, griefer };
     }
 
     async function gameRandomFixture() {
@@ -137,6 +138,28 @@ describe("Mastermind", function () {
         const gameId = findEvent(receipt, "GameCreated").args?._game_id;
 
         return { creator, opponent, griefer, manager, gameId };
+    }
+
+    async function notEmptyStackFixture() {
+        const { creator, opponent, gameId, manager, griefer } = await loadFixture(GameCreatedFixture);
+
+        const stakeAmountA = hre.ethers.parseEther("10.0");
+        const stakeAmountB = hre.ethers.parseEther("100.0");
+        await creator.execFunction("proposeStake",[gameId], {value: stakeAmountA});
+        await opponent.execFunction("proposeStake",[gameId], {value: stakeAmountB});
+
+        return { creator, opponent, griefer, manager, gameId };
+    }
+
+    async function inGameFixture() {
+        const { creator, opponent, gameId, manager, griefer } = await loadFixture(GameCreatedFixture);
+
+        const stakeAmount = hre.ethers.parseEther("10.0");
+        await creator.execFunction("proposeStake",[gameId], {value: stakeAmount});
+        const receipt = await opponent.execFunction("proposeStake",[gameId], {value: stakeAmount});
+        const creator_first_breaker = findEvent(receipt, "GameStart").args._creator_is_first_breaker;
+
+        return { creator, opponent, griefer, manager, gameId, creator_first_breaker };
     }
 
     it("should deploy the contract correctly", async function () {
@@ -242,7 +265,6 @@ describe("Mastermind", function () {
         it("Should handle staking and emit StakeSuccessful event", async function () {
             const { gameId, manager, creator, opponent} = await loadFixture(GameCreatedFixture);
 
-
             // Propose a stake by the creator
             const stakeAmount = hre.ethers.parseEther("1.0");
             await creator.execFunction("proposeStake",[gameId], {value: stakeAmount});
@@ -252,7 +274,13 @@ describe("Mastermind", function () {
                 expect(_game_id).to.equal(gameId);
                 expect(_stake).to.equal(hre.ethers.parseEther("1.0"));
             });
+            await manager.test("GameStart", (_game_id, first_breaker) => {
+                expect(_game_id).to.equal(gameId);
+                expect(first_breaker).to.be.oneOf([true, false]);
+            });
         });
+
+        //TODO aggiungi evento per emit StakeSent, come sopra ma quando il gioco piu avanti in un momento a caso
 
         it("Should handle stake failing and emit StakeFailed event with the failing value", async function () {
             const { manager, creator, opponent, gameId } = await loadFixture(GameCreatedFixture);
@@ -264,31 +292,80 @@ describe("Mastermind", function () {
                 expect(_game_id).to.equal(gameId);
                 expect(_opp_stake).to.equal(hre.ethers.parseEther("2.0"));
             });
-            
+
+            //TODO pending return deve essere controllato che sia aumentato, qui e in ogni StakeFailed
+
             /*const stakeEvent = findEventInGame(receipt, "StakeFailed", gameId);
             expect(stakeEvent.args._game_id).to.equal(gameId);
-            expect(stakeEvent.args._opp_stake).to.equal(hre.ethers.parseEther("2.0"));*/
-            
+            expect(stakeEvent.args._opp_stake).to.equal(hre.ethers.parseEther("2.0"));*/            
+        });
+        
+        it("Should revert with the right error if wanna stack on non existing game", async function () {
+            const { gameId, manager, creator, opponent} = await loadFixture(GameCreatedFixture);
+
+            // Propose a stake by the creator
+            const stakeAmount = hre.ethers.parseEther("1.0");
+            const invalidGameId = "0x1000000000000000000000000000000000000000000000000000000000000000"; // Assuming this game ID does not exist
+
+            await creator.execFunction("proposeStake",[gameId], {value: stakeAmount});
+            await expect(opponent.execFunction("proposeStake",[invalidGameId], {value: stakeAmount})).to.be.revertedWith("[Internal Error] Supplied Game does not exist");
+        });
+
+        it("Should revert with the right error if wanna stack on non staking game", async function () {
+            const { gameId, manager, creator, opponent} = await loadFixture(GameCreatedFixture);
+
+            // Propose a stake by the creator
+            const stakeAmount = hre.ethers.parseEther("1.0");
+
+            await creator.execFunction("proposeStake",[gameId], {value: stakeAmount});
+            await opponent.execFunction("proposeStake",[gameId], {value: stakeAmount});
+            await expect(opponent.execFunction("proposeStake",[gameId], {value: stakeAmount})).to.be.revertedWith("Game not in staking phase");
+        });
+
+        it("Should revert with the right error if outsider player wanna stack on the game", async function () {
+            const { gameId, griefer, creator, opponent} = await loadFixture(GameCreatedFixture);
+
+            // Propose a stake by the creator
+            const stakeAmount = hre.ethers.parseEther("1.0");
+
+            await creator.execFunction("proposeStake",[gameId], {value: stakeAmount});
+            await expect(griefer.execFunction("proposeStake",[gameId], {value: stakeAmount})).to.be.revertedWith("Sender not part of game");
+        });
+
+        it("Should revert with the right error if wanna stack on non staking game", async function () {
+            const { gameId, manager, creator, opponent} = await loadFixture(GameCreatedFixture);
+
+            // Propose a stake by the creator
+            const stakeAmount = hre.ethers.parseEther("1.0");
+
+            await expect(opponent.execFunction("proposeStake",[gameId], {value: stakeAmount})).to.be.revertedWith("Not message sender staking turn");
+            await creator.execFunction("proposeStake",[gameId], {value: stakeAmount});
+            await expect(creator.execFunction("proposeStake",[gameId], {value: stakeAmount})).to.be.revertedWith("Not message sender staking turn");
         });
     });
 
-    // it("should allow setting and guessing the code", async function () {
-    //     const { mastermind, p1, gameId } = await loadFixture(gameCreatedFixture);
+    describe("In Game Management", function () {
+    it("should allow setting and guessing the code", async function () {
+        const { creator, opponent, griefer, manager, gameId, creator_first_breaker } = await loadFixture(inGameFixture);
 
-    //     // Propose a stake by the creator
-    //     const stakeAmount = hre.ethers.utils.parseEther("1.0");
-    //     await mastermind.proposeStake(gameId, { value: stakeAmount });
-    //     await mastermind.connect(p1).proposeStake(gameId, { value: stakeAmount });
+        // Set code hash
+        const codeHash = hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("1234"));
+        if (creator_first_breaker)
+            await creator.execFunction("setCodeHash",[gameId, codeHash]);
+        else
+            await opponent.execFunction("proposeStake",[gameId, codeHash]);
 
-    //     // Set code hash
-    //     const codeHash = hre.ethers.utils.keccak256(hre.ethers.utils.toUtf8Bytes("1234"));
-    //     await mastermind.setCodeHash(gameId, codeHash);
+        await manager.test("SecretSet", (_game_id, curr_turn) => {
+            expect(_game_id).to.equal(gameId);
+            expect(curr_turn).to.not.be.undefined;
+        });
+    });
+
 
     //     // Make a guess
     //     const guess = hre.ethers.utils.formatBytes32String("1234");
     //     const guessTx = await mastermind.connect(p1).guess(gameId, guess);
     //     await guessTx.wait();
-    // });
 
     // it("should handle feedback and game flow", async function () {
     //     const { mastermind, p1, gameId } = await loadFixture(gameCreatedFixture);
@@ -343,4 +420,65 @@ describe("Mastermind", function () {
     //     const winnerEvent = claimRewardReceipt.events?.find((event: any) => event.event === "GameWinner");
     //     expect(winnerEvent).to.not.be.undefined;
     // });
+
+    });
+
+    describe("Withdrawals", function () {
+        describe("Validations", function () {
+    //       it("Should handle stakedfunds and return true", async function () {
+    //         const { creator, opponent, griefer, manager, gameId } = await loadFixture(notEmptyStackFixture);
+    
+    //         await expect(opponent.execFunction("withdraw",[])).to.be.revertedWith("TODO");
+    //       });
+    
+        //   it("Should revert with the right error if called from another account", async function () {
+        //     const { lock, unlockTime, otherAccount } = await loadFixture(deployOneYearLockFixture);
+    
+        //     // We can increase the time in Hardhat Network
+        //     await time.increaseTo(unlockTime);
+    
+        //     // We use lock.connect() to send a transaction from another account
+        //     await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith("You aren't the owner");
+        //   });
+    
+        //   it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
+        //     const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
+    
+        //     // Transactions are sent using the first signer by default
+        //     await time.increaseTo(unlockTime);
+    
+        //     await expect(lock.withdraw()).not.to.be.reverted;
+        //   });
+        });
+    
+    //     describe("Events", function () {
+    //       it("Should emit an event on withdrawals", async function () {
+    //         const { lock, unlockTime, lockedAmount } = await loadFixture(
+    //           deployOneYearLockFixture
+    //         );
+    
+    //         await time.increaseTo(unlockTime);
+    
+    //         await expect(lock.withdraw())
+    //           .to.emit(lock, "Withdrawal")
+    //           .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
+    //       });
+    //     });
+    
+    //     describe("Transfers", function () {
+    //       it("Should transfer the funds to the owner", async function () {
+    //         const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
+    //           deployOneYearLockFixture
+    //         );
+    
+    //         await time.increaseTo(unlockTime);
+    
+    //         await expect(lock.withdraw()).to.changeEtherBalances(
+    //           [owner, lock],
+    //           [lockedAmount, -lockedAmount]
+    //         );
+    //       });
+    //     });
+    //   });
+    });
 });
